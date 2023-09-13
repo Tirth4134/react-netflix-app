b0889dc3f2f391a131b90070794b204e2a375e09
Tirth Patel <108384786+Tirth4134@users.noreply.github.com>
2023-09-13 14:55:56 +0530

    Create File.jsx

---
+++ File.jsx
@@ -0,0 +1,35 @@
+export   const File = [
+    {
+        sname : "money heist",
+        imgsrc : "https://e0.pxfuel.com/wallpapers/989/1006/desktop-wallpaper-money-heist-money-heist-season-1.jpg",
+        title : "netflix original series",
+        link : "https://www.netflix.com/in/title/80192098",
+    },
+    {
+        sname : "all of us are dead",
+        imgsrc : "https://e0.pxfuel.com/wallpapers/286/359/desktop-wallpaper-things-we-absolutely-loved-about-all-of-us-are-dead-metro-style-yoon-gwi-nam-thumbnail.jpg",
+        title : "netflix original series",
+        link : "https://www.netflix.com/in/title/81237994",
+    },
+    {
+        sname :  "dark",
+        imgsrc : "https://w0.peakpx.com/wallpaper/592/913/HD-wallpaper-jonas-dark-dark-netflix-dark-series-everything-is-connected-netflix-triquetra-thumbnail.jpg",
+        title : "netflix original series",
+        link : "https://www.netflix.com/in/title/80100172",
+    },
+    {
+        sname : "stranger things",
+        imgsrc : "https://hollywoodlife.com/wp-content/uploads/2021/11/stranger-things-4-creel-house-netflix-embed.jpg",
+        title : "netflix original series",
+        link : "https://www.netflix.com/in/title/80057281",
+    },
+    {
+        sname :"hellbound",
+        imgsrc : "https://m.media-amazon.com/images/M/MV5BNzU1ODk0ZWUtMzdkYS00YmI2LWI1NjItOTg0NjQxNGY5MmNlXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UX1000_.jpg",
+        title : "netflix original series",
+        link : "https://www.netflix.com/in/title/81256675",
+    },
+    
+]
+
+// export default File;
